a85063621318f6f87bd552c1048442cc270bd808
James Stott <japstott@gmail.com>
2024-03-13 19:37:08 +0000

    create boilerplate footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import Navigation from "./components/Navigation";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Categories from "./components/pages/Categories";
+import Footer from "./components/Footer";
+import FooterTrim from "./components/FooterTrim";
 
 function App() {
   return (
     <Router>
       <Navigation />
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />}></Route>
         <Route path="categories" element={<Categories />}></Route>
       </Routes>
+
+      <Footer />
+      <FooterTrim />
     </Router>
   );
 }
