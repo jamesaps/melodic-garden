5f752d9583d479be4dc63750bdcb4966462b8fac
haydawn <haydawnsds@gmail.com>
2024-03-13 21:42:54 +0000

    Update image sizing

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -8,18 +8,18 @@ const Jumbotron = () => {
     <div className="flex justify-between items-center">
 
       <div className="w-1/3">
-        <img src={plant1} alt="Image 1" className="w-full" />
+        <img src={plant1} alt="Image 1" className="w-full h-96" />
       </div>
 
       <div className="w-1/3">
-        <img src={plant2} alt="Image 2" className="w-full" />
+        <img src={plant2} alt="Image 2" className="w-full h-96" />
       </div>
 
-      <div className="w-1/3 bg-gray-800 text-white p-8">
-        <img  alt="Image" className="absolute inset-0 w-full h-full object-cover" />
-        <div className="relative z-10">
-          <h2 className="text-4xl font-bold mb-4">A Botanial Haven</h2>
-          <p className="text-lg mb-4">A curated collection of lush greenery, carefully chosen to bring vitality and style to your space.</p>
+      <div className="w-1/3 bg-gray-800 text-white p-8 h-96 " >
+        <img src={plant3} alt="Image3" className="relative inset-0 w-48 h-48 object-cover align-content: center" />
+        <div className="z-10">
+          <h2 className="text-2xl font-bold mb-4">A Botanial Haven</h2>
+          <p className="text-sm mb-4">A curated collection of lush greenery, carefully chosen to bring vitality and style to your space.</p>
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">View all House Plants</button>
         </div>
       </div>
