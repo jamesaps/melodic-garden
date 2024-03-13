5d71e13ab7d1d985f48c5c57b09ac2a25e4424b1
adam-deb <152192245+Adam-deb@users.noreply.github.com>
2024-03-13 20:39:30 +0000

    imported router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import ProductDescription from "./components/productDescription";
+import Navigation from "./components/Navigation";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Categories from "./components/pages/Categories";
 
 function App() {
-  return ( 
-  <div className="App">
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-  <ProductDescription/>
-  </div>
-  )
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="categories" element={<Categories />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 export default App;
