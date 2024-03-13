d216ac0161c5b6e5f1a19c33b50b41c6d21566d5
James Stott <japstott@gmail.com>
2024-03-13 20:32:13 +0000

    implement fixed footer trim

--- src/components/FooterTrim.jsx
+++ src/components/FooterTrim.jsx
@@ -1,3 +1,10 @@
+import footerImage from "../assets/footer-image.png";
+
 export default function FooterTrim() {
-  return <></>;
+  return (
+    <div
+      className="w-full h-10 bg-top bg-cover fixed bottom-0"
+      style={{ backgroundImage: `url(${footerImage})` }}
+    ></div>
+  );
 }
