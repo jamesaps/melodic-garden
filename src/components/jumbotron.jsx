82b96fd5f16072cc35f1e2928465d119d0859555
haydawn <haydawnsds@gmail.com>
2024-03-14 13:58:50 +0000

    Fixed some aligment bugs

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -5,25 +5,25 @@ import plant3 from "../assets/plant3.png"
 
 const Jumbotron = () => {
   return (
-    <div className="absolute top-0 left-0 w-full text-white p-8">
-    <div className="flex justify-between items-center">
+    <div className="absolute top-0 left-0 w-full h-full ">
+    <div className="flex items-stretch h-full">
 
-      <div className="w-1/3">
-        <img src={plant1} alt="Image 1" className="w-full h-96" />
+      <div className="w-1/3 hidden md:flex ">
+        <img src={plant1} alt="Image 1" className="w-full h-full" />
       </div>
 
-      <div className="w-1/3">
-        <img src={plant2} alt="Image 2" className="w-full h-96" />
+      <div className="w-1/3 hidden md:flex">
+        <img src={plant2} alt="Image 2" className="w-full h-full" />
       </div>
 
-      <div className="w-1/3 bg-green-900 text-white p-8 h-96 " >
-      <div className="w-full h-48">
-        <img src={plant3} alt="Image3" className="relative inset-0 w-full h-48 object-cover rounded-lg mb-8 " />
+      <div className="md:w-1/3 bg-green-900 p-6 flex flex-col justify-between " >
+      <div className="mb-6 ">
+        <img src={plant3} alt="Image3" className="w-full rounded-lg " />
         </div>
-        <div className="z-10">
-          <h2 className="text-2xl font-bold mb-4 flex justify-start">A Botanial Haven</h2>
-          <p className="text-sm mb-4 text-left">A curated collection of lush greenery, carefully chosen to bring vitality and style to your space.</p>
-          <button className=" w-full bg-white hover:bg-blue-600 text-green-950 text-sm shadow-2xl py-2 px-4 rounded-full">View all House Plants</button>
+        <div className="z-10 mb-4 text-white">
+          <h2 className="text-2xl font-bold mb-4 text-left">A Botanial Haven</h2>
+          <p className="text-sm mb-6 text-left">A curated collection of lush greenery, carefully chosen to bring vitality and style to your space.</p>
+          <button className=" w-full bg-white hover:bg-blue-600 text-green-950 text-sm shadow-md  py-2 px-4 rounded-full">View all House Plants</button>
         </div>
       </div>
     </div>
