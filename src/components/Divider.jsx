ab719c77995127509a68dded3d8e543c4de093be
haydawn <haydawnsds@gmail.com>
2024-03-15 20:40:06 +0000

    removed divider from container

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Divider = () => {
     return (
-        <div className="containe h-screen"> {/* I used h-screen for viewing in dev mode, needs to be removed when ready to use compo*/}
             <div className="relative w-full">
              <div className="absolute inset-0"> 
                  <section className="w-full bg-cover bg-center py-40 md:py-40 text-white relative opacity-90" style={{backgroundImage: "url('https://images.pexels.com/photos/6373479/pexels-photo-6373479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"}}>
@@ -13,7 +12,6 @@ const Divider = () => {
                 </section>
              </div>
             </div>
-        </div>
       );
     };
 
