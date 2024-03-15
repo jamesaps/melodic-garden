f8e3c501803f0ef51ff454634b853e77c62d09af
haydawn <haydawnsds@gmail.com>
2024-03-15 20:35:40 +0000

    Center Text on divider

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -6,9 +6,9 @@ const Divider = () => {
             <div className="relative w-full">
              <div className="absolute inset-0"> 
                  <section className="w-full bg-cover bg-center py-40 md:py-40 text-white relative opacity-90" style={{backgroundImage: "url('https://images.pexels.com/photos/6373479/pexels-photo-6373479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"}}>
-                     <div className="absolute p-10 inset-0 bg-green-900 opacity-90">
+                     <div className="absolute text-center p-10 inset-0 bg-green-900 opacity-90">
                          <h1 className="text-3xl md:text-5xl font-medium mb-1">Explore our diverse selection, ranging from vibrant succulents to majestic ferns. </h1>
-                         <p className="text-base md:text-lg p-3 md:p-8">A curated collection of lush greenery, carefully chosen to bring vitality and style to your space </p>
+                         <p className="text-base md:text-lg p-2 md:p-8">A curated collection of lush greenery, carefully chosen to bring vitality and style to your space </p>
                     </div>
                 </section>
              </div>
