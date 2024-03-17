0557a161febc73cdfe464c42c26c4a227b25fe18
sophieainsw <sophieainsworth3@gmail.com>
2024-03-17 14:28:29 +0000

    added fetch from JSON to create different cards on homepage

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,18 +1,17 @@
 // import React from 'react';
 
-function productCard() {
-    return (
-        <div className="max-w-sm bg-white rounded-lg p-a">
-          <a href="#">
-            <img className="rounded-3xl mb-4 w-full h-auto" src="https://img.freepik.com/free-photo/calathea-plant-gray-pot_53876-133131.jpg?t=st=1710359315~exp=1710362915~hmac=55aa0c17f0e85688fd8f6e085696f872ebaeb80dca2e6da9b7457e2761625aac&w=826" alt="" />
-          
-            <div className="flex items-center justify-between mt-8">
-              <span className="text-md md:text-xl lg:text-2xl text-gray-900 dark:text-white ml-7 mb-5">Calathea</span>
-              <span className="text-md md:text-xl lg:text-2xl font-bold text-gray-900 dark:text-white mr-7 mb-5">£25.00</span>
-            </div>
-          </a>
+function ProductCard({ product }) {
+  return (
+    <div className="max-w-sm bg-white rounded-lg p-4">
+      <a href="#">
+        <img className="rounded-3xl mb-4 w-full h-auto" src={product.Image} alt={product.Name} />
+        <div className="flex items-center justify-between mt-8">
+          <span className="text-md md:text-xl lg:text-2xl text-gray-900 dark:text-white ml-7 mb-5 mr-2">{product.Name}</span>
+          <span className="text-md md:text-xl lg:text-2xl font-bold text-gray-900 dark:text-white mr-7 mb-5">{product.Price}</span>
         </div>
-      );
-    }
+      </a>
+    </div>
+  );
+}
 
-export default productCard;
+export default ProductCard;
