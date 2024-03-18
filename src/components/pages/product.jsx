bee1fab887399cfe5add2f80646b521b5e60ccca
James Stott <japstott@gmail.com>
2024-03-18 16:48:32 +0000

    revert Product export code

--- src/components/pages/product.jsx
+++ src/components/pages/product.jsx
@@ -3,7 +3,7 @@ import ProductDescription from "../ProductDescription";
 import RecommendedProducts from "../RecommendedProducts";
 import { useProducts } from "../../hooks/useProducts";
 
-export default function Product() {
+const Product = () => {
   const { id } = useParams();
   const { products } = useProducts();
 
@@ -20,4 +20,6 @@ export default function Product() {
       <RecommendedProducts otherPlants={otherPlants} />
     </>
   );
-}
+};
+
+export default Product;
