0f0b14c0320f19c80cba5d550c3cae02ef7e90ba
sophieainsw <sophieainsworth3@gmail.com>
2024-03-19 19:51:30 +0000

    added components to homepage

---
+++ src/components/HomePlantSection.jsx
@@ -0,0 +1,34 @@
+
+// import React from 'react';
+import FeatureCard from "./FeatureCard";
+import ProductCard from "./productCard";
+import products from '../plants.json';
+
+function HomePlantSection() {
+
+    const getRandomProducts = (arr, n) => {
+        const random = arr.sort(() => 0.5 - Math.random());
+        return random.slice(0, n);
+    };
+
+    const randomProducts = getRandomProducts(products, 6);
+
+    return (
+        <div className="container mx-auto gap-4 grid grid-cols-2 grid-rows-2 sm:grid-cols-2 md:grid-cols-4 pt-12 pb-12">
+            <div className="col-span-1 md:col-span-1 lg:col-span-1 row-span-2 flex justify-center items-center">
+                <FeatureCard />
+            </div>
+            {randomProducts.map((product) => (
+                <div key={product.Id} className="col-span-1">
+                    <div className="flex justify-center items-center">
+                        <div style={{ width: '75%', height: '75%' }}>
+                            <ProductCard product={product} />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default HomePlantSection;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Jumbotron from "../Jumbotron.jsx";
 import Divider from "../Divider.jsx";
+import HomePlantSection from "../HomePlantSection.jsx";
+import CategoryCard from "../CategoryCard.jsx";
 
 export default function Home() {
   return (
     <div>
       <Jumbotron />
+      <HomePlantSection/>
       <Divider />
+      <CategoryCard/>
     </div>
   );
 }
