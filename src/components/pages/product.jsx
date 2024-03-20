d1d066344d965d8c00634c380d96ca1d48020e5b
adam-deb <152192245+Adam-deb@users.noreply.github.com>
2024-03-20 18:41:17 +0000

    changed Product.jsx

--- src/components/pages/product.jsx
+++ src/components/pages/product.jsx
@@ -12,7 +12,7 @@ const Product = () => {
     .filter(
       (plant) => plant.Size === mainProduct.Size && plant.Id !== parseInt(id),
     )
-    .slice(0, 3);
+  
 
     return (
       <div className="mb-12 mt-0 flex max-w-6xl flex-col gap-4 sm:mt-24 md:mt-48 md:gap-8 lg:gap-12">
