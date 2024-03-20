578573c0fd163b3eef5fa5b9f280f2597fb816aa
adam-deb <152192245+Adam-deb@users.noreply.github.com>
2024-03-20 18:47:11 +0000

    made changes to Product.jsx

--- src/components/pages/product.jsx
+++ src/components/pages/product.jsx
@@ -14,12 +14,16 @@ const Product = () => {
     )
     .slice(0, 3);
 
-  return (
-    <>
-      <ProductDescription mainProduct={mainProduct} />
-      <RecommendedProducts otherPlants={otherPlants} />
-    </>
-  );
-};
 
+    return (
+      <div className="mb-12 mt-0 flex max-w-6xl flex-col gap-4 sm:mt-24 md:mt-48 md:gap-8 lg:gap-12">
+        <ProductDescription mainProduct={mainProduct} />
+        <RecommendedProducts
+          mainProduct={mainProduct}
+          otherPlants={otherPlants}
+        />
+      </div>
+    );
+  };
+  
 export default Product;
